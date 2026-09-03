1087af984ab476537debcd082d0680c976562a72
milenasantostds-gif <milena.santos.tds@gmail.com>
2026-09-03 18:04:47 -0300

    1065

---
+++ 1065.cs
@@ -0,0 +1,17 @@
+
+using namespace std;
+
+int main() {
+    int valor, pares = 0;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> valor;
+
+        if (valor % 2 == 0) {
+            pares++;
+       Console.Writelilner("valores pares")
+
+    return 0;
+}
+    }
+}
